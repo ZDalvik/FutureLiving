bf819be28c5fbfad0d1cd5eaec89956037d09500
ZDalvik <pakymatto@gmail.com>
2024-05-08 16:58:53 +0200

    Create MeteoDomotics.ino

---
+++ MeteoDomotics/MeteoDomotics.ino
@@ -0,0 +1,16 @@
+/*
+  IoT project for a automatic light and curtain opener and closer
+  based on enviromental variables took from a ESP32 module.
+  This project is for an IoT lab class final exam.
+  Università degli studi della Campania Luigi Vanvitelli dipartimento di ingegneria.
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
